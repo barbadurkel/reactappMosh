059efcdd7c70397d55da3dd60a345e0c4df84afb
moubella <sudofalcon@gmail.com>
2019-07-12 16:41:52 +0200

    added a delete button + refacto

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-### ReactJS Tips and tricks:
+# React Tips and tricks:
 
 
-# Props vs State
-# Props includes data that we GIVE to a component
-# State includes data that is LOCAL to that component
+*Props vs State*
+- Props includes data that we GIVE to a component
+- State includes data that is LOCAL to that component
+- Props are READONLY inside a component
+- RULE : The component that owns a piece of the state should be the one to modify it (example: deleting a counter is handled by the Counters component BUT the 'event' of deletion is raised in the Counter component)
+- props can be propertis or (even better) OBJECTS
+- Components RAISE events and Parent-components HANDLE those events (onDelete, handleDelete) 
+- RULE : Keep states only in Parent components
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,14 +9,37 @@ import Counter from './counter'
             { id : 3, value : 0},
             { id : 4, value : 0}
         ]
+    };
+
+handleDelete = counterId => {
+        const counters = this.state.counters.filter( c => c.id !== counterId);
+        this.setState({counters: counters})
     }
+handleReset = () => {
+        const counters = this.state.counters.map( c => {c.value = 0; return c});
+        this.setState({ counters })
+}
+handleIncrement = () => {
+        // cloning
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index]= {...counter};
+        counters[index].value++;
+
+
+}
+
     render() {
         return (
             <div>
+              <button className="btn btn-primary btn-sm m-2" onClick={this.handleReset}>Reset</button>
+
               {this.state.counters.map( counter => 
-                     <Counter key={counter.id} value={counter.value}>
-                         <h6>Counter number <b>#{counter.id}</b></h6></Counter> 
-                 )}
+                     <Counter key={counter.id} 
+                     onDelete={this.handleDelete} 
+                     onIncrement={this.handleIncrement}
+                     counter={counter}/> 
+              )}
             </div>
         );
     }
